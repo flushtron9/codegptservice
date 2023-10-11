bf28d16e140aed133918e456b186f5ae9067426d
flushtron9 <suresh.kada@gmail.com>
2023-10-11 19:32:31 -0400

    Structure

---
+++ src/main/java/com/wimthackathon/codegptservice/CodegptserviceApplication.java
@@ -0,0 +1,13 @@
+package com.wimthackathon.codegptservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegptserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodegptserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wimthackathon/codegptservice/CodegptserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimthackathon.codegptservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodegptserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
